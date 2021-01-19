b68d2aff16306a22a9b39d5ec2786c08c3b471c0
mpadge <mark.padgham@email.com>
2021-01-19 10:12:15 +0100

    finish roclet

--- R/rssr_roclet.R
+++ R/rssr_roclet.R
@@ -1,3 +1,22 @@
+
+#' Parse tests tag
+#' 
+#' @param x Input
+#' 
+#' @return Parsed tag
+#' 
+#' @importFrom roxygen2 roxy_tag_parse
+#' @export
+roxy_tag_parse.roxy_tag_rssr <- function(x) {
+    roxygen2::tag_markdown (x)
+}
+
+#' @importFrom roxygen2 roxy_tag_rd
+#' @export
+roxy_tag_rd.roxy_tag_rssr <- function(x, base_path, env) {
+  NULL
+}
+
 #' rssr_roclet
 #'
 #' Get values of all `rssr` tags in function documentation
@@ -17,28 +36,27 @@ roclet_process.roclet_rssr <- function (x, blocks, env, base_path) { # nolint
     msgs <- list ()
 
     for (block in blocks) {
-        block_obj <- block$object
 
         if (length (roxygen2::block_get_tags (block, "rssr")) == 0L) {
             # Block does not have @rssr
             next
         }
 
-        func_name <- block_obj$alias
-
-        file_nm <- ""
-        if (!is.null (attr (block, "filename"))) {
-            file_nm <- paste0(" [in '", attr (block, "filename"), "']: ")
-        }
+        func_name <- block$object$alias
+        standards <- roxygen2::block_get_tag_value (block, "rssr")
 
         block_title <- roxygen2::block_get_tag_value (block, "title")
-        msg <- paste0 ("Function '", func_name,
-                       "()' with title '", block_title, "'", file_nm)
+        block_backref <- basename (roxygen2::block_get_tag_value (block, "backref"))
+        block_line <- block$line
+        msg <- paste0 ("Standards [", standards, "] in function '",
+                       func_name, "()' on line#",
+                       block_line, " of file [",
+                       block_backref, "]")
         msgs <- c (msgs, msg)
     }
 
     if (length (msgs) > 0L) {
-        message ("Functions with `rssr` tags:")
+        message ("rOpenSci Statistical Software Standards:")
         message (paste0 ("  * ", msgs, collapse = "\n"), sep = "")
     }
 
